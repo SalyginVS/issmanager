a5ab81b538f296bfe02c62c72cb446bb99c845d4
SalyginVS <salyginvs@gmail.com>
2021-03-25 18:01:34 +0200

    added users user block and activate buttons

---
+++ issmanager/src/Model/User/UseCase/Activate/Command.php
@@ -0,0 +1,21 @@
+<?php
+
+
+namespace App\Model\User\UseCase\Activate;
+
+namespace App\Model\User\UseCase\Activate;
+
+use Symfony\Component\Validator\Constraints as Assert;
+
+class Command
+{
+    /**
+     * @Assert\NotBlank()
+     */
+    public $id;
+
+    public function __construct(string $id)
+    {
+        $this->id = $id;
+    }
+}
---
+++ issmanager/src/Model/User/UseCase/Block/Command.php
@@ -0,0 +1,19 @@
+<?php
+
+
+namespace App\Model\User\UseCase\Block;
+
+use Symfony\Component\Validator\Constraints as Assert;
+
+class Command
+{
+    /**
+     * @Assert\NotBlank()
+     */
+    public $id;
+
+    public function __construct(string $id)
+    {
+        $this->id = $id;
+    }
+}
