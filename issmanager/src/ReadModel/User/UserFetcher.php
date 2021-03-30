3603c49aa4977a696da49fc9826d2cadb87fdc9d
SalyginVS <salyginvs@gmail.com>
2021-03-30 18:39:44 +0300

    removed unused code

--- issmanager/src/ReadModel/User/UserFetcher.php
+++ issmanager/src/ReadModel/User/UserFetcher.php
@@ -40,7 +40,7 @@ class UserFetcher
                 ->from('user_users')
                 ->where('reset_token_token = :token')
                 ->setParameter(':token', $token)
-                ->execute()->fetchColumn(0) > 0;
+                ->execute()->fetchColumn() > 0;
     }
 
 
