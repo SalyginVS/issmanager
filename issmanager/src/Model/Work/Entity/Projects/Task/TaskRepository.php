a448169fa58edfa8662cd29d3c2206a5ec02e4f8
SalyginVS <salyginvs@gmail.com>
2021-06-04 12:17:25 +0300

    added move usecase

--- issmanager/src/Model/Work/Entity/Projects/Task/TaskRepository.php
+++ issmanager/src/Model/Work/Entity/Projects/Task/TaskRepository.php
@@ -20,6 +20,15 @@ class TaskRepository
         $this->connection = $em->getConnection();
     }
 
+    /**
+     * @param Id $id
+     * @return Task[]
+     */
+    public function allByParent(Id $id): array
+    {
+        return $this->repo->findBy(['parent' => $id->getValue()]);
+    }
+
     public function get(Id $id): Task
     {
         /** @var Task $task */
